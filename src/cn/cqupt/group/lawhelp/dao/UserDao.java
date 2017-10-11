08568f6bc69c5a07964b3297475a4a0443e39afd
Krisez <740879437@qq.com>
2017-10-11 21:15:50 +0800

    startup

--- src/cn/cqupt/group/lawhelp/dao/UserDao.java
+++ src/cn/cqupt/group/lawhelp/dao/UserDao.java
@@ -1,5 +1,25 @@
 package cn.cqupt.group.lawhelp.dao;
 
-public class UserDao {
+import cn.cqupt.group.lawhelp.entity.User;
+import cn.cqupt.group.lawhelp.utils.MyConn;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDao {
+    public static String queryUsers(){
+        Connection connection = null;
+        try{
+            connection = MyConn.getConnection();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        List<User> list = new ArrayList<User>();
+
+        String result = JSON.toJSONString(list);
+        return result;
+    }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,16 +1,10 @@
-<%--
+<%@ page import="cn.cqupt.group.lawhelp.dao.UserDao" %><%--
   Created by IntelliJ IDEA.
   User: Krisez
   Date: 2017/9/26
   Time: 20:20
   To change this template use File | Settings | File Templates.
 --%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<html>
-  <head>
-    <title>BackClient</title>
-  </head>
-  <body align="center">
-  后台客户端
-  </body>
-</html>
+<%@ page contentType="json;charset=UTF-8" language="java" %>
+<% response.getWriter().write("adadsdasd");%>
+
